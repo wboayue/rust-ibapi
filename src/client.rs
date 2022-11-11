3e163d14f646406037a61e7ea8b2d4888949097f
Wil Boayue <wilfred.boayue@airbus-sv.com>
2022-11-10 20:09:37 -0800

    cleanup

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,4 @@
-use anyhow::{anyhow, Result};
-
-use crate::domain::BidAsk;
-use crate::domain::{Contract, ContractDetails, RealTimeBar, TagValue};
+use anyhow;
 
 #[derive(Debug)]
 pub struct Client<'a> {
@@ -17,12 +14,4 @@ pub fn connect(host: &str, port: i32, client_id: i32) -> anyhow::Result<Client> 
         port,
         client_id,
     })
-}
-
-impl Client<'_> {
-    /// Requests contract information.
-    /// This method will provide all the contracts matching the contract provided. It can also be used to retrieve complete options and futures chains. This information will be returned at EWrapper:contractDetails. Though it is now (in API version > 9.72.12) advised to use reqSecDefOptParams for that purpose.
-    pub fn contract_details(&self, contract: &Contract) -> Result<ContractDetails> {
-        Err(anyhow!("not implemented!"))
-    }
 }
