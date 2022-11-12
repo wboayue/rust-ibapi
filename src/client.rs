d90a14b03f1ab57e2df31936055587ffa65d97d1
Wil Boayue <wilfred.boayue@airbus-sv.com>
2022-11-11 22:36:52 -0800

    getting there

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,7 @@
-use anyhow;
+use anyhow::{anyhow, Result};
+use time::OffsetDateTime;
+
+use crate::domain::Contract;
 
 #[derive(Debug)]
 pub struct BasicClient<'a> {
@@ -9,6 +12,24 @@ pub struct BasicClient<'a> {
 
 pub struct Packet {}
 pub struct PacketIterator {}
+
+pub trait ToPacket {
+    fn to_packet(&self) -> String;
+}
+
+impl Packet {
+    pub fn add_field<T: ToPacket>(&self, val: T) {
+        val.to_packet();
+    }
+
+    pub fn next_int(&self) -> Result<i32> {
+        Err(anyhow!("not implemented!"))
+    }
+
+    pub fn next_date_time(&self) -> Result<OffsetDateTime> {
+        Err(anyhow!("not implemented!"))
+    }
+}
 
 pub trait Client {
     fn next_request_id(&self) -> i32;
@@ -27,4 +48,34 @@ pub fn connect(host: &str, port: i32, client_id: i32) -> anyhow::Result<BasicCli
         port,
         client_id,
     })
+}
+
+impl ToPacket for bool {
+    fn to_packet(&self) -> String {
+        "bool".to_string()
+    }
+}
+
+impl ToPacket for String {
+    fn to_packet(&self) -> String {
+        self.clone()
+    }
+}
+
+impl ToPacket for i32 {
+    fn to_packet(&self) -> String {
+        "i32".to_string()
+    }
+}
+
+impl ToPacket for &str {
+    fn to_packet(&self) -> String {
+        self.clone().to_string()
+    }
+}
+
+impl ToPacket for &Contract {
+    fn to_packet(&self) -> String {
+        "contract".to_string()
+    }
 }
--- src/historical_market_data.rs
+++ src/historical_market_data.rs
@@ -13,65 +13,128 @@ struct RequestHeadTimestamp<'a> {
 }
 
 impl RequestHeadTimestamp<'_> {
+    const message_id: i32 = 32;
+
     fn encode(&self) -> Result<Packet> {
-        Err(anyhow!("not implemented!"))
+        let mut packet = Packet {};
+
+        packet.add_field(Self::message_id);
+        packet.add_field(self.request_id);
+        packet.add_field(self.contract);
+        packet.add_field(self.use_rth);
+        packet.add_field(self.what_to_show);
+        packet.add_field("format_date");
+
+        Ok(packet)
     }
 }
 
-struct ReceiveHeadTimestamp {
-}
-
-impl ReceiveHeadTimestamp {
-    fn decode(packet: &Packet) -> Result<ReceiveHeadTimestamp> {
-        Err(anyhow!("not implemented!"))
-    }
-}
+// https://github.com/InteractiveBrokers/tws-api/blob/master/source/csharpclient/client/EClient.cs
+// https://github.com/InteractiveBrokers/tws-api/blob/master/source/csharpclient/client/EDecoder.cs#L733
 
 /// Returns the timestamp of earliest available historical data for a contract and data type.
-pub fn head_timestamp<C: Client>(client: &C, contract: &Contract, what_to_show: &str, use_rth: bool) -> Result<OffsetDateTime> {
-    let request = RequestHeadTimestamp{
+pub fn head_timestamp<C: Client>(
+    client: &C,
+    contract: &Contract,
+    what_to_show: &str,
+    use_rth: bool,
+) -> Result<OffsetDateTime> {
+    // if (!CheckServerVersion(MinServerVer.REQ_HEAD_TIMESTAMP,
+    //     " It does not support head time stamp requests."))
+    //     return;
+
+    let request = RequestHeadTimestamp {
         request_id: client.next_request_id(),
         contract,
         what_to_show,
-        use_rth
+        use_rth,
     };
 
     client.send_packet(&request.encode()?);
+
     let packet = client.receive_packet(request.request_id);
 
-    ReceiveHeadTimestamp::decode(&packet);
+    decode_head_timestamp(&packet)
+}
 
-    Err(anyhow!("not implemented!"))
+fn decode_head_timestamp(packet: &Packet) -> Result<OffsetDateTime> {
+    let _request_id = packet.next_int()?;
+    let head_timestamp = packet.next_date_time()?;
+
+    Ok(head_timestamp)
 }
 
 /// Returns data histogram of specified contract
-pub fn histogram_data<C: Client>(client: &C, contract: &Contract, use_rth: bool, period: &str) -> Result<HistogramDataIterator> {
+pub fn histogram_data<C: Client>(
+    client: &C,
+    contract: &Contract,
+    use_rth: bool,
+    period: &str,
+) -> Result<HistogramDataIterator> {
     // " S (seconds) - " D (days)
     // " W (weeks) - " M (months)
     // " Y (years)
     Err(anyhow!("not implemented!"))
 }
 
-pub fn historical_data<C: Client>(client: &C, contract: &Contract, end: &OffsetDateTime, duration: &str, bar_size: &str, what_to_show: &str, use_rth: bool, keep_up_to_date: bool) -> Result<BarIterator> {
-/// https://interactivebrokers.github.io/tws-api/historical_bars.html#hd_duration
-/// https://interactivebrokers.github.io/tws-api/historical_bars.html#hd_barsize
-/// https://interactivebrokers.github.io/tws-api/historical_bars.html#hd_what_to_show
+pub fn historical_data<C: Client>(
+    client: &C,
+    contract: &Contract,
+    end: &OffsetDateTime,
+    duration: &str,
+    bar_size: &str,
+    what_to_show: &str,
+    use_rth: bool,
+    keep_up_to_date: bool,
+) -> Result<BarIterator> {
+    // https://interactivebrokers.github.io/tws-api/historical_bars.html#hd_duration
+    // https://interactivebrokers.github.io/tws-api/historical_bars.html#hd_barsize
+    // https://interactivebrokers.github.io/tws-api/historical_bars.html#hd_what_to_show
     Err(anyhow!("not implemented!"))
 }
 
-pub fn historical_schedule<C: Client>(client: &C, contract: &Contract, use_rth: bool, period: &str) -> Result<HistogramDataIterator> {
+pub fn historical_schedule<C: Client>(
+    client: &C,
+    contract: &Contract,
+    use_rth: bool,
+    period: &str,
+) -> Result<HistogramDataIterator> {
     Err(anyhow!("not implemented!"))
 }
 
-pub fn historical_ticks<C: Client>(client: &C, contract: &Contract, start_date: Option<OffsetDateTime>, end_date: Option<OffsetDateTime>, number_of_ticks: i32, use_rth: i32, ignore_size: bool) -> Result<HistoricalTickIterator> {
+pub fn historical_ticks<C: Client>(
+    client: &C,
+    contract: &Contract,
+    start_date: Option<OffsetDateTime>,
+    end_date: Option<OffsetDateTime>,
+    number_of_ticks: i32,
+    use_rth: i32,
+    ignore_size: bool,
+) -> Result<HistoricalTickIterator> {
     Err(anyhow!("not implemented!"))
 }
 
-pub fn historical_ticks_bid_ask<C: Client>(client: &C, contract: &Contract, start_date: Option<OffsetDateTime>, end_date: Option<OffsetDateTime>, number_of_ticks: i32, use_rth: i32, ignore_size: bool) -> Result<HistoricalTickBidAskIterator> {
+pub fn historical_ticks_bid_ask<C: Client>(
+    client: &C,
+    contract: &Contract,
+    start_date: Option<OffsetDateTime>,
+    end_date: Option<OffsetDateTime>,
+    number_of_ticks: i32,
+    use_rth: i32,
+    ignore_size: bool,
+) -> Result<HistoricalTickBidAskIterator> {
     Err(anyhow!("not implemented!"))
 }
 
-pub fn historical_ticks_last<C: Client>(client: &C, contract: &Contract, start_date: Option<OffsetDateTime>, end_date: Option<OffsetDateTime>, number_of_ticks: i32, use_rth: i32, ignore_size: bool) -> Result<HistoricalTickLastIterator> {
+pub fn historical_ticks_last<C: Client>(
+    client: &C,
+    contract: &Contract,
+    start_date: Option<OffsetDateTime>,
+    end_date: Option<OffsetDateTime>,
+    number_of_ticks: i32,
+    use_rth: i32,
+    ignore_size: bool,
+) -> Result<HistoricalTickLastIterator> {
     Err(anyhow!("not implemented!"))
 }
 
@@ -133,8 +196,17 @@ pub struct Bar {
 }
 
 pub struct BarIterator {}
-/// https://interactivebrokers.github.io/tws-api/classIBApi_1_1Bar.html
+// https://interactivebrokers.github.io/tws-api/classIBApi_1_1Bar.html
 
 pub struct HistoricalSchedule {
-//    string startDateTime, string endDateTime, string timeZone, HistoricalSession[]
+    //    string startDateTime, string endDateTime, string timeZone, HistoricalSession[]
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
 }
