5d4801c12b8cde2832d7bdcd2fb3fcbe65f8858c
Wil Boayue <wboayue@gmail.com>
2022-12-13 21:40:15 -0800

    checkpoint

--- src/orders.rs
+++ src/orders.rs
@@ -1,3 +1,9 @@
+use std::fmt::Debug;
+
+use anyhow::{anyhow, Result};
+
+use crate::client::{Client, RequestPacket, ResponsePacket};
+use crate::contracts::{Contract};
 
 
 // place_order
@@ -6,12 +12,18 @@ pub fn place_order<C: Client + Debug>(
     order_id: i32,
     contract: &Contract,
     order: i32,
-) -> Result<()>> {
+) -> Result<()> {
+    Ok(())
 }
 
 // cancel_order
 pub fn cancel_order<C: Client + Debug>(
     client: &mut C,
     order_id: i32,
-) -> Result<()>> {
+) -> Result<()> {
+    Ok(())
+}
+
+pub fn check_order_status<C: Client + Debug>() {
+    
 }
