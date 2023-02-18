421b6866725343d0bcc09ea7bccfd2c3c3e0c6cb
Wil Boayue <wilfred.boayue@airbus-sv.com>
2023-02-18 12:38:47 -0800

    checkpoint

---
+++ src/bin/stream_bars.rs
@@ -0,0 +1,59 @@
+use clap::{arg, ArgMatches, Command};
+
+use ibapi::client::IBClient;
+use ibapi::contracts::Contract;
+use ibapi::market_data::streaming;
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let matches = Command::new("stream_bars")
+        .version("1.0")
+        .author("Wil Boayue <wil@wsbsolutions.com")
+        .about("Streams realtime bars")
+        .arg(arg!(--connection_string <VALUE>).default_value("odin:4002"))
+        .arg(arg!(--stock <SYMBOL>))
+        .arg(arg!(--futures <SYMBOL>))
+        .get_matches();
+
+    let connection_string = matches
+        .get_one::<String>("connection_string")
+        .expect("connection_string is required");
+    let contract = extract_contract(&matches).expect("error parsing --stock or --future");
+
+    println!("connection_string: {connection_string:?}");
+    println!("contract: {contract:?}");
+
+    let mut client = IBClient::connect("odin:4002")?;
+
+    let bars = streaming::realtime_bars(&mut client, &contract)?;
+    for (i, bar) in bars.iter().enumerate() {
+        println!("bar: {i:?} {bar:?}");
+
+        if i > 60 {
+            break;
+        }
+    }
+
+    // let mut contract = Contract::stock(stock_symbol);
+    // contract.currency = "USD".to_string();
+    // debug!("contract template: {contract:?}");
+
+    Ok(())
+}
+
+fn extract_contract(matches: &ArgMatches) -> Option<Contract> {
+    if matches.contains_id("stock") {
+        let symbol = matches
+            .get_one::<String>("stock")
+            .expect("error parsing stock symbol");
+
+        return Some(Contract::stock(&symbol.to_uppercase()));
+    } else {
+        let symbol = matches
+            .get_one::<String>("futures")
+            .expect("error parsing futures symbol");
+
+        return Some(Contract::futures(&symbol.to_uppercase()));
+    }
+}
--- src/market_data/streaming.rs
+++ src/market_data/streaming.rs
@@ -1,1 +1,52 @@
+use std::fmt::Debug;
+use std::string::ToString;
 
+use anyhow::{anyhow, Result};
+use log::{error, info};
+
+use crate::client::{Client, RequestMessage, ResponseMessage};
+use crate::contracts::{Contract, ContractDetails};
+use crate::messages::{IncomingMessages, OutgoingMessages};
+use crate::server_versions;
+
+/// Requests contract information.
+///
+/// This method will provide all the contracts matching the contract provided. It can also be used to retrieve complete options and futures chains. Though it is now (in API version > 9.72.12) advised to use reqSecDefOptParams for that purpose.
+///
+/// # Arguments
+/// * `client` - [Client] with an active connection to gateway.
+/// * `contract` - The [Contract] used as sample to query the available contracts. Typically, it will contain the [Contract]'s symbol, currency, security_type, and exchange.
+///
+/// # Examples
+///
+/// ```no_run
+/// use ibapi::client::IBClient;
+/// use ibapi::contracts::{self, Contract};
+/// use ibapi::market_data::streaming;
+///
+/// fn main() -> anyhow::Result<()> {
+///     let mut client = IBClient::connect("localhost:4002")?;
+///
+///     let contract = Contract::stock("TSLA");
+///     let bars = streaming::realtime_bars(&mut client, &contract)?;
+///
+///     for bar in &bars {
+///         println!("bar: {bar:?}");
+///     }
+///
+///     Ok(())
+/// }
+/// ```
+pub fn realtime_bars<C: Client + Debug>(
+    client: &mut C,
+    contract: &Contract,
+) -> Result<Vec<ContractDetails>> {
+    // if !contract.security_id_type.is_empty() || !contract.security_id.is_empty() {
+    //     client.check_server_version(
+    //         server_versions::SEC_ID_TYPE,
+    //         "It does not support security_id_type or security_id attributes",
+    //     )?
+    // }
+
+    Ok(Vec::default())
+}
