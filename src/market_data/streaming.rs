c65ebfce5f90d296c6faae8c500e15e41ace8427
Wil Boayue <wilfred.boayue@airbus-sv.com>
2023-02-20 17:22:51 -0800

    fixed tests

--- src/market_data/streaming.rs
+++ src/market_data/streaming.rs
@@ -13,6 +13,39 @@ use crate::server_versions;
 
 use super::{BarSize, RealTimeBar, WhatToShow};
 
+/// Requests realtime bars.
+///
+/// This method will provide all the contracts matching the contract provided. It can also be used to retrieve complete options and futures chains. Though it is now (in API version > 9.72.12) advised to use reqSecDefOptParams for that purpose.
+///
+/// # Arguments
+/// * `client` - [Client] with an active connection to gateway.
+/// * `contract` - The [Contract] used as sample to query the available contracts. Typically, it will contain the [Contract]'s symbol, currency, security_type, and exchange.
+///
+/// # Examples
+///
+/// ```no_run
+/// use ibapi::client::IBClient;
+/// use ibapi::contracts::{self, Contract};
+/// use ibapi::market_data::{streaming, BarSize, WhatToShow};
+///
+/// fn main() -> anyhow::Result<()> {
+///     let mut client = IBClient::connect("localhost:4002")?;
+///
+///     let contract = Contract::stock("TSLA");
+///     let bars = streaming::realtime_bars(&mut client, &contract, &BarSize::Secs5, &WhatToShow::Trades, false)?;
+///
+///     for (i, bar) in bars.enumerate() {
+///         println!("bar[{i}]: {bar:?}");
+///
+///         if i > 60 {
+///             break;
+///         }
+///     }
+///
+///
+///     Ok(())
+/// }
+/// ```
 pub fn realtime_bars<C: Client + Debug>(
     client: &mut C,
     contract: &Contract,
@@ -30,34 +63,6 @@ pub fn realtime_bars<C: Client + Debug>(
     )
 }
 
-/// Requests contract information.
-///
-/// This method will provide all the contracts matching the contract provided. It can also be used to retrieve complete options and futures chains. Though it is now (in API version > 9.72.12) advised to use reqSecDefOptParams for that purpose.
-///
-/// # Arguments
-/// * `client` - [Client] with an active connection to gateway.
-/// * `contract` - The [Contract] used as sample to query the available contracts. Typically, it will contain the [Contract]'s symbol, currency, security_type, and exchange.
-///
-/// # Examples
-///
-/// ```no_run
-/// use ibapi::client::IBClient;
-/// use ibapi::contracts::{self, Contract};
-/// use ibapi::market_data::streaming;
-///
-/// fn main() -> anyhow::Result<()> {
-///     let mut client = IBClient::connect("localhost:4002")?;
-///
-///     let contract = Contract::stock("TSLA");
-///     let bars = streaming::realtime_bars(&mut client, &contract)?;
-///
-///     for bar in &bars {
-///         println!("bar: {bar:?}");
-///     }
-///
-///     Ok(())
-/// }
-/// ```
 pub fn realtime_bars_with_options<C: Client + Debug>(
     client: &mut C,
     contract: &Contract,
