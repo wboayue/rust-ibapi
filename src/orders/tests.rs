031791a68feb295ab89e1f5686632aba2b328b0c
Wil Boayue <wboayue@gmail.com>
2023-02-23 08:42:32 -0800

    tests: limit order

--- src/orders/tests.rs
+++ src/orders/tests.rs
@@ -14,11 +14,39 @@ fn place_market_order() {
 
     let results = super::place_order(&mut client, order_id, &contract, &order);
 
-    println!("results: {results:?}");
-
     assert_eq!(
         client.request_messages[0],
         "3|12|0||FUT|202303|0|||EUREX||EUR|FGBL MAR 23||||BUY|10|MKT|||||||0||0|0|0|0|0|0|0|0||0||||||||0||0|0|||0|||0|0||||||||0|||||0|||||||||||0|||0|0|||0||0|0||0|0|0|0|0|0|||||||||0|0|0|0|||0|"
+    );
+
+    assert!(
+        results.is_ok(),
+        "failed to place order: {:?}",
+        results.unwrap_err()
+    );
+}
+
+#[test]
+fn place_limit_order() {
+    let mut client = ClientStub::new(server_versions::SIZE_RULES);
+
+    client.response_messages = vec![];
+
+    let order_id = 12;
+    let contract = contract_samples::future_with_local_symbol();
+    let order = order_builder::limit_order(super::Action::Buy, 10.0, 500.00);
+
+    let results = super::place_order(&mut client, order_id, &contract, &order);
+
+    assert_eq!(
+        client.request_messages[0],
+        "3|12|0||FUT|202303|0|||EUREX||EUR|FGBL MAR 23||||BUY|10|LMT|500||||||0||0|0|0|0|0|0|0|0||0||||||||0||0|0|||0|||0|0||||||||0|||||0|||||||||||0|||0|0|||0||0|0||0|0|0|0|0|0|||||||||0|0|0|0|||0|"
+    );
+
+    assert!(
+        results.is_ok(),
+        "failed to place order: {:?}",
+        results.unwrap_err()
     );
 }
 
@@ -34,10 +62,14 @@ fn place_combo_market_order() {
 
     let results = super::place_order(&mut client, order_id, &contract, &order);
 
-    println!("order: {results:?}");
-
     assert_eq!(
         client.request_messages[0],
         "3|12|0|WTI|BAG||0|||SMART||USD|||||SELL|150|MKT|||||||0||0|0|0|0|0|0|0|0|2|55928698|1|BUY|IPE|0|0||0|55850663|1|SELL|IPE|0|0||0|0|1|NonGuaranteed|1||0||||||||0||0|0|||0|||0|0||||||||0|||||0|||||||||||0|||0|0|||0||0|0||0|0|0|0|0|0|||||||||0|0|0|0|||0|"
+    );
+
+    assert!(
+        results.is_ok(),
+        "failed to place order: {:?}",
+        results.unwrap_err()
     );
 }
