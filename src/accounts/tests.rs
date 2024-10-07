ffb1b79ab3e339486ef9b9b90517a04e35a1e701
Wil Boayue <wboayue@gmail.com>
2024-10-06 17:03:39 -0700

    fixed tests

--- src/accounts/tests.rs
+++ src/accounts/tests.rs
@@ -1,6 +1,6 @@
 use std::sync::{Arc, Mutex, RwLock};
 
-use crate::{accounts::pnl, server_versions, stubs::MessageBusStub, Client};
+use crate::{server_versions, stubs::MessageBusStub, Client};
 
 #[test]
 fn test_pnl() {
@@ -11,14 +11,32 @@ fn test_pnl() {
 
     let client = Client::stubbed(message_bus, server_versions::SIZE_RULES);
 
-    let account = "xyzzx";
-    let model_code = Some("A");
+    let account = "DU1234567";
+    let model_code = Some("TARGET2024");
 
-    let _responses = client.pnl(account, model_code).expect("msg");
+    let _ = client.pnl(account, model_code).expect("request pnl failed");
 
     let request_messages = client.message_bus.lock().expect("MessageBus is poisoned").request_messages();
 
-    assert_eq!(request_messages[0].encode_simple(), "92|9000|xyzzx|A|");
+    assert_eq!(request_messages[0].encode_simple(), "92|9000|DU1234567|TARGET2024|");
+}
 
-    // assert!(responses.is_ok(), "failed to place order: {}", results.err().unwrap());
+#[test]
+fn test_pnl_single() {
+    let message_bus = Arc::new(Mutex::new(MessageBusStub {
+        request_messages: RwLock::new(vec![]),
+        response_messages: vec![],
+    }));
+
+    let client = Client::stubbed(message_bus, server_versions::SIZE_RULES);
+
+    let account = "DU1234567";
+    let contract_id = 1001;
+    let model_code = Some("TARGET2024");
+
+    let _ = client.pnl_single(account, contract_id, model_code).expect("request pnl failed");
+
+    let request_messages = client.message_bus.lock().expect("MessageBus is poisoned").request_messages();
+
+    assert_eq!(request_messages[0].encode_simple(), "94|9000|DU1234567|TARGET2024|1001|");
 }
