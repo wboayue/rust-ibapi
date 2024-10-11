a94a769eb7a277798ced3dbe0ed324bcd4b67c52
Wilfred Boayue <wilfred.boayue@airbus-sv.com>
2024-10-11 16:17:15 -0700

    updated tests

--- src/accounts/tests.rs
+++ src/accounts/tests.rs
@@ -15,10 +15,15 @@ fn test_pnl() {
     let model_code = Some("TARGET2024");
 
     let _ = client.pnl(account, model_code).expect("request pnl failed");
+    let _ = client.pnl(account, None).expect("request pnl failed");
 
     let request_messages = client.message_bus.lock().unwrap().request_messages();
 
     assert_eq!(request_messages[0].encode_simple(), "92|9000|DU1234567|TARGET2024|");
+    assert_eq!(request_messages[1].encode_simple(), "93|9000|");
+
+    assert_eq!(request_messages[2].encode_simple(), "92|9001|DU1234567||");
+    assert_eq!(request_messages[3].encode_simple(), "93|9001|");
 }
 
 #[test]
@@ -35,8 +40,54 @@ fn test_pnl_single() {
     let model_code = Some("TARGET2024");
 
     let _ = client.pnl_single(account, contract_id, model_code).expect("request pnl failed");
+    let _ = client.pnl_single(account, contract_id, None).expect("request pnl failed");
 
     let request_messages = client.message_bus.lock().unwrap().request_messages();
 
     assert_eq!(request_messages[0].encode_simple(), "94|9000|DU1234567|TARGET2024|1001|");
+    assert_eq!(request_messages[1].encode_simple(), "95|9000|");
+
+    assert_eq!(request_messages[2].encode_simple(), "94|9001|DU1234567||1001|");
+    assert_eq!(request_messages[3].encode_simple(), "95|9001|");
+}
+
+#[test]
+fn test_positions() {
+    let message_bus = Arc::new(Mutex::new(MessageBusStub {
+        request_messages: RwLock::new(vec![]),
+        response_messages: vec![],
+    }));
+
+    let client = Client::stubbed(message_bus, server_versions::SIZE_RULES);
+
+    let _ = client.positions().expect("request positions failed");
+
+    let request_messages = client.message_bus.lock().unwrap().request_messages();
+
+    assert_eq!(request_messages[0].encode_simple(), "61|1|");
+    assert_eq!(request_messages[1].encode_simple(), "64|1|");
+}
+
+#[test]
+fn test_positions_multi() {
+    let message_bus = Arc::new(Mutex::new(MessageBusStub {
+        request_messages: RwLock::new(vec![]),
+        response_messages: vec![],
+    }));
+
+    let client = Client::stubbed(message_bus, server_versions::SIZE_RULES);
+
+    let account = Some("DU1234567");
+    let model_code = Some("TARGET2024");
+
+    let _ = client.positions_multi(account, model_code).expect("request positions failed");
+    let _ = client.positions_multi(None, model_code).expect("request positions failed");
+
+    let request_messages = client.message_bus.lock().unwrap().request_messages();
+
+    assert_eq!(request_messages[0].encode_simple(), "74|1|9000|DU1234567|TARGET2024|");
+    assert_eq!(request_messages[1].encode_simple(), "75|1|9000|");
+
+    assert_eq!(request_messages[2].encode_simple(), "74|1|9001||TARGET2024|");
+    assert_eq!(request_messages[3].encode_simple(), "75|1|9001|");
 }
