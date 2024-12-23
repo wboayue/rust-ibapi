b80cacf50ae67a9c1e0737cd5b7946a17670fe54
Wil Boayue <wboayue@gmail.com>
2024-12-22 23:46:26 -0800

    cleanup

--- examples/tick_by_tick_last.rs
+++ examples/tick_by_tick_last.rs
@@ -1,4 +1,3 @@
-use std::thread;
 use std::time::Duration;
 
 use ibapi::contracts::Contract;
@@ -15,11 +14,13 @@ fn main() {
 
     let client = Client::connect(connection_string, 100).expect("connection failed");
 
-    // let contract = contract_es();
     let contract = Contract::stock("NVDA");
     let ticks = client.tick_by_tick_last(&contract, 0, false).expect("failed to get ticks");
 
-    println!("streaming last price for security_type: {:?}, symbol: {}", contract.security_type, contract.symbol);
+    println!(
+        "streaming last price for security_type: {:?}, symbol: {}",
+        contract.security_type, contract.symbol
+    );
 
     for (i, tick) in ticks.timeout_iter(Duration::from_secs(10)).enumerate() {
         match tick {
@@ -27,6 +28,7 @@ fn main() {
                 println!("{}: {i:?} {trade:?}", contract.symbol);
             }
             TickLast::Notice(notice) => {
+                // server could send a notice if it doesn't recognize the contract
                 println!("error_code: {}, error_message: {}", notice.code, notice.message);
             }
         }
@@ -35,21 +37,5 @@ fn main() {
     // check for errors during streaming
     if ticks.error().is_some() {
         println!("error: {}", ticks.error().unwrap());
-    }    
-}
-
-fn contract_es() -> Contract {
-    let mut contract = Contract::futures("ES");
-    contract.local_symbol = "ESU3".to_string();
-    contract.trading_class = "ES".into();
-    contract.exchange = "CME".into();
-    contract
-}
-
-fn contract_gc() -> Contract {
-    let mut contract = Contract::futures("GC");
-    contract.exchange = "COMEX".to_owned();
-    contract.local_symbol = "GCZ3".to_string();
-    contract.trading_class = "GC".into();
-    contract
+    }
 }
