e5f57dc46102b2855e9723a7ec20a2835cf78b80
Wil Boayue <wboayue@gmail.com>
2025-01-03 15:12:03 -0500

    cleanup error handling

--- examples/wsh_event_data_by_filter.rs
+++ examples/wsh_event_data_by_filter.rs
@@ -8,13 +8,29 @@ fn main() {
 
     let client = Client::connect("127.0.0.1:4002", 100).expect("connection failed");
 
-    let filter = ""; // filter as JSON string.
+    let filter = r#"
+    {
+        "country": "All",
+        "watchlist": ["8314"],
+        "limit_region": 10,
+        "limit": 10,
+        "wshe_ed": "true",
+        "wshe_bod": "true"
+    }    
+    "#;
+
+    let filter = filter.replace("\n", "").to_string();
 
     let subscription = client
-        .wsh_event_data_by_filter(filter, None, None)
+        .wsh_event_data_by_filter(&filter, None, None)
         .expect("request wsh event data failed");
 
-    for event_data in subscription {
+    for event_data in &subscription {
         println!("{:?}", event_data);
+    }
+
+    // check for errors
+    if let Some(error) = subscription.error() {
+        println!("error: {}", error);
     }
 }
