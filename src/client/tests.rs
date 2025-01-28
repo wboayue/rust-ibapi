df358250e513c3a00c96fad7988b107c7ada61f2
Wilfred Boayue <wilfred.boayue@airbus-sv.com>
2025-01-28 15:02:47 -0800

    added accessor for client id

--- src/client/tests.rs
+++ src/client/tests.rs
@@ -1,1 +1,17 @@
+use std::sync::Arc;
 
+use crate::{stubs::MessageBusStub, transport::ConnectionMetadata};
+
+use super::Client;
+
+
+#[test]
+fn test_client_id() {
+    let client_id = 500;
+    let connection_metadata = ConnectionMetadata{client_id, ..ConnectionMetadata::default()};
+    let message_bus = Arc::new(MessageBusStub::default());
+
+    let client = Client::new(connection_metadata, message_bus).unwrap();
+
+    assert_eq!(client.client_id(), client_id);
+}
