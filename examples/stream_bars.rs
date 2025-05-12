62fa58e25190d24a8bc259f06342fc68a6c843f1
Petel__ <chunkitmax1125@gmail.com>
2025-05-11 18:14:28 -0700

    update contract contractor for futures and crypto to use local symbol and exchange

--- examples/stream_bars.rs
+++ examples/stream_bars.rs
@@ -17,15 +17,16 @@ fn main() -> anyhow::Result<()> {
         .arg(arg!(--connection_string <VALUE>).default_value("localhost:4002"))
         .arg(arg!(--stock <SYMBOL>))
         .arg(arg!(--futures <SYMBOL>))
+        .arg(arg!(--exchange <EXCHANGE>))
         .get_matches();
 
     let connection_string = matches.get_one::<String>("connection_string").expect("connection_string is required");
-    let contract = extract_contract(&matches).expect("error parsing --stock or --future");
+    let contract = extract_contract(&matches).expect("error parsing --stock or --future or --exchange");
 
     println!("connection_string: {connection_string:?}");
     println!("contract: {contract:?}");
 
-    let client = Client::connect("127.0.0.1:4002", 100).unwrap();
+    let client = Client::connect("192.168.1.2:4002", 100).unwrap();
 
     println!("server_version: {}", client.server_version());
     println!("server_time: {:?}", client.connection_time());
@@ -44,10 +45,11 @@ fn main() -> anyhow::Result<()> {
 
 fn extract_contract(matches: &ArgMatches) -> Option<Contract> {
     if let Some(symbol) = matches.get_one::<String>("stock") {
-        Some(Contract::stock(&symbol.to_uppercase()))
-    } else {
-        matches
-            .get_one::<String>("futures")
-            .map(|symbol| Contract::futures(&symbol.to_uppercase()))
+        return Some(Contract::stock(&symbol.to_uppercase()));
+    } else if let Some(local_symbol) = matches.get_one::<String>("futures") {
+        if let Some(exchange) = matches.get_one::<String>("exchange") {
+            return Some(Contract::futures(&local_symbol.to_uppercase(), &exchange.to_uppercase()));
+        }
     }
+    None
 }
