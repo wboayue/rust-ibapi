04012f4938dc341ae4daad059cc851aa1a6ed560
Wil Boayue <wil.boayue@gmail.com>
2025-07-11 17:20:49 -0700

    allow bool assert comparison

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@
 // Allow octal-looking escapes in string literals (used in test data)
 #![allow(clippy::octal_escapes)]
 
+#![allow(clippy::bool_assert_comparison)]
+
 // Feature guards - async takes precedence over sync
 #[cfg(not(any(feature = "sync", feature = "async")))]
 compile_error!(
