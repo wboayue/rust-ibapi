9c75e6d7f11acfc0481e40004e68994f19101659
Wil Boayue <wil.boayue@gmail.com>
2026-05-11 15:54:58 -0700

    market_data/tests: drop redundant import + catch-all duplicates

--- src/market_data/tests.rs
+++ src/market_data/tests.rs
@@ -1,5 +1,4 @@
 use super::*;
-use crate::market_data::encoders::encode_request_market_data_type;
 use crate::messages::OutgoingMessages;
 
 #[test]
@@ -26,13 +25,11 @@ fn market_data_type_from_i32() {
     assert_eq!(MarketDataType::from(3), MarketDataType::Delayed);
     assert_eq!(MarketDataType::from(4), MarketDataType::DelayedFrozen);
     assert_eq!(MarketDataType::from(0), MarketDataType::Unknown);
-    assert_eq!(MarketDataType::from(99), MarketDataType::Unknown);
-    assert_eq!(MarketDataType::from(-1), MarketDataType::Unknown);
 }
 
 #[test]
 fn encode_request_market_data_type_round_trip() {
-    let bytes = encode_request_market_data_type(MarketDataType::Delayed).unwrap();
+    let bytes = encoders::encode_request_market_data_type(MarketDataType::Delayed).unwrap();
     let msg_id = i32::from_be_bytes([bytes[0], bytes[1], bytes[2], bytes[3]]);
     assert_eq!(msg_id, OutgoingMessages::RequestMarketDataType as i32 + 200);
 
